38ec58982a461a2448697fb12ca964cecdaa635c
rifayet <rifayetrimon88@gmail.com>
2025-05-09 21:09:04 +0800

    flutter project setup

---
+++ lib/pages/welcome/splash.jsx
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Simulate a delay for the splash screen
+    Future.delayed(const Duration(seconds: 2), () {
+      // Navigate to the next page after the delay
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/logo/app_icon.png'),
+      ),
+    );
+  }
+}
